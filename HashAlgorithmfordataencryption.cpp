4462a9508e3e89210a551941a86a8f1f20c52884
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2022-11-27 14:55:14 +0000

    first made 25th of nov.
    works mostly now integrating more encryption and encryption and function for containers

--- HashAlgorithmfordataencryption.cpp
+++ HashAlgorithmfordataencryption.cpp
@@ -103,7 +103,7 @@ static class Key {
 #define keyvaluecheck 0000000;
 #ifndef keyvaluecheck 1||>0;
 #endif >0;
-
+	 
 	int main()
 	{
 		const struct HashStructure
@@ -122,72 +122,83 @@ static class Key {
 			virtual int callHashing() = 0;
 		};
 		static KeyGeneration* keyA{};
+
 		std::string keyval;
 		std::string hh = keyA->encryptionKey<std::string>(keyval); //encrypt the key holder;
 		// hh holds data for now.
 		//secure this lambda
 
-		keyval.assign(hh) = keyval;
+		auto hash = [](std::string const& s) {
+			std::hash<std::string> hash;
+			return hash(s);
+		};
+		std::cout << "Hashed key as hh: " << hash;
+		std::cout << "'\nkey will now enter more levels of encyrption'\n";
+		//key val now needs to be copied into keyval
+		keyval = hash(hh);
 		if (keyval != "") {
-			delete& hh;
+			// hh;
 		}
-		auto key_ = [StorageSecure]() {
-			auto _key = StorageSecure.key;
+		else { 
+			auto key_ = [StorageSecure]() { //alll will be refactored into headers and cpp files temp lambdas for now but will be used when needed
+				auto _key = StorageSecure.key;
 
-			double& kn = _key;
+				double& kn = _key;
 
-			//simple just for experimenting with probability and certainty of values for longer guesses needed!.
-			//this is the non hash function stuff for simple maths to build up from.
+				//simple just for experimenting with probability and certainty of values for longer guesses needed!.
+				//this is the non hash function stuff for simple maths to build up from.
+				// 
+				//NEED TO SORT OUT DELETE LOGIC.
+				for (auto as = 0; as < 10 * (sizeof(kn) % 2 * kn); as++) {
+					kn = (0x22 * static_cast<unsigned long long>(as) ^ 2902) % (long&)StorageSecure - (int&)as % sizeof(malloc(_key));
+					//now pass kn into a algorithm may be slow asf but who caress. thats refactoring shit.
+					auto scramble = [kn]() {
+						long double scramblekn = kn;
+						switch (scramblekn != 0) {
+							case sizeof(kn) < sizeof(scramblekn) :
+								struct memory {
+								virtual long double memoryallocation() = 0;
+								virtual::double_t Swish() = 0;
+							};
 
-			for (auto as = 0; as < 10 * (sizeof(kn) % 2 * kn); as++) {
-				kn = (0x22 * static_cast<unsigned long long>(as) ^ 2902) % (long&)StorageSecure - (int&)as % sizeof(malloc(_key));
-				//now pass kn into a algorithm may be slow asf but who caress. thats refactoring shit.
-				auto scramble = [kn]() {
-					long double scramblekn = kn;
-					switch (scramblekn != 0) {
-						case sizeof(kn) < sizeof(scramblekn) :
-							struct memory {
-							virtual long double memoryallocation() = 0;
-							virtual::double_t Swish() = 0;
-						};
+							struct locmem :memory {
+							private:
+								long double memoryallocation() {
+									long double Now;
+									if (!0) {
 
-						struct locmem :memory {
-						private:
-							long double memoryallocation() {
-								long double Now;
-								if (!0) {
-
+									}
+									if (NULL || !nullptr) {//never be nullptr but it will be in future and evaluated before the creation of Now and used logixally some how.
+										Now = 0;
+									}			//experiemental lambdas will all be changed just early build!..
+									else {// need to refactor for zero  semantic errors!!!
+										//compiles fine for now until deeper use/implementation of function and setting.
+										auto mix = [Now](auto q8202) {
+											uint64_t keynumber = q8202; //binary bit 64
+											q8202* keynumber = q8202;
+											keynumber% q8202 = keynumber;
+											auto temp = keynumber;
+											delete keynumber;
+											double long keynumber = temp ^= ((uint64_t)temp ^= temp % ((int&)temp &= temp)) ^ ((long&)temp %= ++sizeof(temp));
+											keynumber = q8202 * keynumber;
+											for (int a = 0; a < keynumber; a++) {
+												keynumber = keynumber ^ 1.3782929;
+												a >= 20 / sizeof(a * keynumber) ? a + keynumber = Now : a++;
+												if (Now != 0 && Now > 120000) {
+													return Now;
+												};
+											}
+										};
+									}
 								}
-								if (NULL || !nullptr) {//never be nullptr but it will be in future and evaluated before the creation of Now and used logixally some how.
-									Now = 0;
-								}			//experiemental lambdas will all be changed just early build!..
-								else {// need to refactor for zero  semantic errors!!!
-									//compiles fine for now until deeper use/implementation of function and setting.
-									auto mix = [Now](auto q8202) {
-										uint64_t keynumber = q8202; //binary bit 64
-										q8202* keynumber = q8202;
-										keynumber% q8202 = keynumber;
-										auto temp = keynumber;
-										delete keynumber;
-										double long keynumber = temp ^= ((uint64_t)temp ^= temp % ((int&)temp &= temp)) ^ ((long&)temp %= ++sizeof(temp));
-										keynumber = q8202 * keynumber;
-										for (int a = 0; a < keynumber; a++) {
-											keynumber = keynumber ^ 1.3782929;
-											a >= 20 / sizeof(a * keynumber) ? a + keynumber = Now : a++;
-											if (Now != 0 && Now > 120000) {
-												return Now;
-											};
-										}
-									};
-								}
-							}
-						};
+							};
 
-					}
-				};
+						}
+					};
 
-			}
-			return _key;
+				}
+				return _key;
+			};
 		};
 		delete keyA;
 		std::cout << "This is the key value for now: " << keyval;
@@ -195,7 +206,6 @@ static class Key {
 		switch (hh.size() || hh.empty() != 0) {
 			delete& keyval;
 		}
-
 	}
 
 
